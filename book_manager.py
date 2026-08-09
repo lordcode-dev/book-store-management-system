78e299f0cd7008de1fd74b5efba979085f498b5e
lordcode-dev <mrbeast765389@gmail.com>
2026-08-09 12:59:01 +0300

    Implement book management functions in book_manager.py
    Added function definitions for managing books in a CSV file.

--- book_manager.py
+++ book_manager.py
@@ -1,1 +1,48 @@
+import csv
+import os
+from tabulate import tabulate
 
+FILE_NAME = "books.csv"
+FIELDNAMES = ["ID", "Title", "Author", "Price"]
+# Create the CSV file if it does not exist
+def initialize_file():
+
+# Read all books from the CSV file
+def read_books():
+
+# Validate Book ID
+def get_valid_id():
+
+# Validate Title or Author
+def get_valid_text(field_name):
+
+# Validate Price
+def get_valid_price():
+
+# Check whether a book ID already exists
+def book_exists(book_id):
+
+# Add a new book
+def add_book():
+    
+# View all books
+def view_books():
+   
+# Search for a book by ID
+def search_book():
+
+# Update a book
+def update_book():
+    
+# Delete a book
+def delete_book():
+   
+# Display the main menu
+def display_menu():
+    
+#Exit the program
+def exit():
+
+#for invalid input
+def invalid():
+  
